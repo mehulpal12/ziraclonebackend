f9e9cc29b248c8343d9e4ca52efe24b5eaef0257
mehulpal12 <89767204+mehulpal12@users.noreply.github.com>
2025-12-08 17:05:37 +0530

    done

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,23 +1,39 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, verifyEmail, refreshAccessToken, forgotPasswordRequest, resetForgotPassword, getCurrentUser, changeCurrentPassword, resendEmailVerification } from "../controller/auth.controller.js";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  verifyEmail,
+  refreshAccessToken,
+  forgotPasswordRequest,
+  resetForgotPassword,
+  getCurrentUser,
+  changeCurrentPassword,
+  resendEmailVerification,
+} from "../controller/auth.controller.js";
 import { validate } from "../middleware/validator.middleware.js";
-import { userRegistrationValidator, userLoginValidator, } from "../validators/index.js";
+import {
+  userRegistrationValidator,
+  userLoginValidator,
+} from "../validators/index.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
-router.route("/register").post(userRegistrationValidator(), validate, registerUser);
-router.route("/login").post( userLoginValidator() , validate ,loginUser );
-router.route("/verify-email/:verificationToken").get( verifyEmail );
-router.route("/refresh-token").post( refreshAccessToken );
-router.route("/forgot-password").post( forgotPasswordRequest );
-router.route("/reset-password/:resetToken").post( resetForgotPassword );
-
+router
+  .route("/register")
+  .post(userRegistrationValidator(), validate, registerUser);
+router.route("/login").post(userLoginValidator(), validate, loginUser);
+router.route("/verify-email/:verificationToken").get(verifyEmail);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/forgot-password").post(forgotPasswordRequest);
+router.route("/reset-password/:resetToken").post(resetForgotPassword);
 
 // protected route
-router.route("/logout").post(  verifyJWT , logoutUser );
-router.route("/current-user").post(  verifyJWT , getCurrentUser );
-router.route("/change-password").post(  verifyJWT , changeCurrentPassword );
-router.route("/resent-email-verification").post(  verifyJWT , resendEmailVerification );
+router.route("/logout").post(verifyJWT, logoutUser);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/change-password").post(verifyJWT, changeCurrentPassword);
+router
+  .route("/resent-email-verification")
+  .post(verifyJWT, resendEmailVerification);
 export default router;
-
